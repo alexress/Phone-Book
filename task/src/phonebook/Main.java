01dde904438b4ecfe57dbd0712931753b3ef49a2
Alexander Ress <a.ress@teckpro.de>
2022-11-08 11:28:32 +0100

    added method to read files

--- task/src/phonebook/Main.java
+++ task/src/phonebook/Main.java
@@ -1,7 +1,27 @@
 package phonebook;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+    }
+    public static ArrayList<String> fileToList(String filename) {
+        ArrayList<String> result = new ArrayList<String>();
+        try{
+            Scanner scanner = new Scanner(new File(filename));
+            while (scanner.hasNext()) {
+                result.add(scanner.nextLine())
+            }
+        } catch (IOException e) {
+            System.out.println("File not found.");
+        } catch (Exception e) {
+            System.out.println("Unexpected Error");
+            System.out.println(e.getMessage());
+        }
+        return result;
     }
 }
