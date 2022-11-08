eb46d57a23032983fe754017175524b9c7174248
Alexander Ress <a.ress@teckpro.de>
2022-11-08 13:08:01 +0100

    Added Bubblesort

--- task/src/phonebook/Main.java
+++ task/src/phonebook/Main.java
@@ -7,11 +7,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-    ArrayList<String> data =
-        fileToList("C:\\Users\\a.ress\\test\\directory.txt");
-    ArrayList<String> find = fileToList("C:\\Users\\a.ress\\test\\find.txt");
+        ArrayList<String> data = fileToList("C:\\Users\\a.ress\\test\\directory.txt");
+        ArrayList<String> find = fileToList("C:\\Users\\a.ress\\test\\find.txt");
+
+        //Linear Search
         int count = 0;                                                      //counts number of found instances
-        System.out.println("Start searching...");
+        System.out.println("Start searching... (linear search)");
         long startTime = System.currentTimeMillis();
         for (String query: find) {
             if (linearSearchContains(query, data)) {
@@ -22,8 +23,10 @@ public class Main {
         long mins = (endTime-startTime) / 60000;
         long secs = ((endTime-startTime) % 60000) / 1000;
         long msecs = (endTime-startTime) % 1000;
-        System.out.printf("Found %d / %d entries. Time taken: %d min. %d sec. %d ms",
+        System.out.printf("Found %d / %d entries. Time taken: %d min. %d sec. %d ms.",
                 count, find.size(), mins, secs, msecs);
+
+        //Bubble Sort + Jump Search
     }
     public static ArrayList<String> fileToList(String filename) {
         ArrayList<String> result = new ArrayList<String>();
@@ -57,5 +60,29 @@ public class Main {
             }
         }
         return false;
+    }
+
+    public static void bubbleSort(ArrayList<Comparable> list) {
+        while (!isSorted(list)) {
+            for (int i = 0; i < list.size()-1; i++) {
+                if (list.get(i).compareTo(list.get(i + 1)) > 0) {
+                    swap(list, i, i + 1);
+                }
+            }
+        }
+    }
+    public static void swap(ArrayList<Comparable> list, int i1, int i2) {
+        Comparable temp = list.get(i1);
+        list.set(i1, list.get(i2));
+        list.set(i2,temp);
+    }
+
+    public static boolean isSorted(ArrayList<Comparable> list) {
+        for (int i = 0; i < list.size()-1; i++) {
+            if (list.get(i).compareTo(list.get(i + 1)) > 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
